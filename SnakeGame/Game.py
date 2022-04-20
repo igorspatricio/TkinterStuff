921d7a38d2a53964e1ea74d597f9e5f26b4d582a
Igor <igorsouzapatricio@gmail.com>
2022-04-20 13:27:54 -0300

    Refatoração

--- SnakeGame/Game.py
+++ SnakeGame/Game.py
@@ -6,39 +6,43 @@ from Comida import *
 WIDTH = 800
 HEIGHT = 500
 TAMANHO = 10
-root = Tk()
 
-cobrinha = Cobra(10)
-comida = Comida(10, WIDTH, HEIGHT)
+if __name__ == '__main__':
+    root = Tk()
+    cobrinha = Cobra(10)
+    comida = Comida(10, WIDTH, HEIGHT)
 
-canva = Canvas(width=WIDTH, height=HEIGHT,  bg = "green")
+    canva = Canvas(width=WIDTH, height=HEIGHT,  bg = "green")
 
-root.bind('<Left>', cobrinha.nova_direcao)
-root.bind('<Up>', cobrinha.nova_direcao)
-root.bind('<Right>', cobrinha.nova_direcao)
-root.bind('<Down>', cobrinha.nova_direcao)
-
-
-canva.pack()
-
-mover = True
-while(cobrinha.get_viva()):
-    canva.after(20)
-    for i in range(len(cobrinha.corpo)):
-        canva.create_rectangle(cobrinha.getCoords(i), fill="red", outline="red")
-    canva.create_rectangle(comida.getCoords(), fill='white', outline='white')
-
-    x,y = comida.get_posicao()
-    if(cobrinha.verifica_comida(x,y)):
-        comida.nova_posicao()
-        cobrinha.addCorpo()
-    if(mover):
-        cobrinha.mover(WIDTH, HEIGHT)
-    mover = not(mover)
-
-    canva.update()
-    canva.delete('all')
+    root.bind('<Left>', cobrinha.nova_direcao)
+    root.bind('<Up>', cobrinha.nova_direcao)
+    root.bind('<Right>', cobrinha.nova_direcao)
+    root.bind('<Down>', cobrinha.nova_direcao)
 
 
-root.mainloop()
+    canva.pack()
+
+    mover = True
+
+    while(cobrinha.get_viva()):
+        canva.after(20)
+
+        for i in range(len(cobrinha.corpo)):
+            canva.create_rectangle(cobrinha.getCoords(i), fill="red", outline="red")
+
+        canva.create_rectangle(comida.getCoords(), fill='white', outline='white')
+
+        x,y = comida.get_posicao()
+        if(cobrinha.verifica_comida(x,y)):
+            comida.nova_posicao()
+            cobrinha.addCorpo()
+
+        if(mover):
+            cobrinha.mover(WIDTH, HEIGHT)
+        mover = not(mover)
+
+        canva.update()
+        canva.delete('all')
+
+        root.mainloop()
 
